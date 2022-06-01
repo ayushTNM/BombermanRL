11ac971705f001d8de6cd19a16fbd40a90e3b069
Josef <91252200+Josef-Hlink@users.noreply.github.com>
2022-06-01 09:45:35 +0200

    Update genplots.sh

--- src/genplots.sh
+++ src/genplots.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python3 menu.py -d5 -C7 -r10 -e200
-python3 menu.py -d6 -C7 -r10 -e200
-python3 menu.py -d7 -C7 -r10 -e200
-python3 menu.py -d8 -C6 -r10 -e200
-python3 menu.py -d9 -C6 -r10 -e200
-python3 menu.py -d10 -C6 -r10 -e200
+# the script that was ran to produce the results published in our paper
+
+python3 menu.py -d5 -C7 -r10 -e200 -o plot
+python3 menu.py -d6 -C7 -r10 -e200 -o plot
+python3 menu.py -d7 -C7 -r10 -e200 -o plot
+python3 menu.py -d8 -C6 -r10 -e200 -o plot
+python3 menu.py -d9 -C6 -r10 -e200 -o plot
+python3 menu.py -d10 -C6 -r10 -e200 -o plot
